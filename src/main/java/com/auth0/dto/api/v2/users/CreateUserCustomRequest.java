dec8778ea7f61c2ef475f9c6f6f38e9969e8289b
Juan Pablo Garbora <juan.garbora@gobio.com>
2021-08-05 13:20:28 -0500

    lombok removed

--- src/main/java/com/auth0/dto/api/v2/users/CreateUserCustomRequest.java
+++ src/main/java/com/auth0/dto/api/v2/users/CreateUserCustomRequest.java
@@ -1,17 +1,7 @@
 package com.auth0.dto.api.v2.users;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CreateUserCustomRequest extends CreateUserRequest {
 
     @JsonProperty("role")
